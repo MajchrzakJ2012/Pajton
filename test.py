d6960dcb47914e8a1acee6f4acd6d03bb7f3bd45
MajchrzakJ2012 <102522039+MajchrzakJ2012@users.noreply.github.com>
2022-06-06 13:29:16 +0200

    top 10 kalkulatorow

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
+a = float (input("podaj a: "))
+b = float (input("podaj b: "))
+def dodawanie(a, b):
+    suma = a+b
+    print ("suma liczb a i b wynosi:", suma)
+def odejmowanie(a, b):
+    roznica = a-b
+    print ("roznica liczb a i b wynosi:", roznica)
+def mnozenie(a,b):
+    iloczyn=a*b
+    print ("iloczyn liczb a i b wynosi", iloczyn)
+def dzielenie(a,b):
+    iloraz=a/b
+    print ("iloraz liczb a i b wynosi", iloraz)
 
+    
+dodawanie(a, b)
+odejmowanie(a, b)
+mnozenie(a,b)
+dzielenie(a,b)
